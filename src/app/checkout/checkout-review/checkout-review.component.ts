ad56607561e2629069ab2de6148c9d7faddf1d4d
Youssef Moghazy <youssefmoghazy55@gmail.com>
2025-06-29 14:16:26 +0300

    update the pop up

--- src/app/checkout/checkout-review/checkout-review.component.ts
+++ src/app/checkout/checkout-review/checkout-review.component.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
+import { HttpClient } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-checkout-review',
@@ -13,19 +15,69 @@ import { IBasket } from 'src/app/shared/models/basket';
 export class CheckoutReviewComponent implements OnInit {
   @Input() appStepper: CdkStepper;
   basket$: Observable<IBasket>;
+  timeLeft: number = 120; // 2 minutes in seconds
+  interval: any;
+  Math = Math;
 
-  constructor(private basketService: BasketService, private toastr: ToastrService) { }
+  constructor(
+    private basketService: BasketService,
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
   }
 
   createPaymentIntent() {
-    return this.basketService.createPaymentIntent().subscribe((response: any) => {
-      this.appStepper.next();
-    }, error => {
-      console.log(error);
-    })
+    return this.basketService.createPaymentIntent().subscribe(
+      (response: any) => {
+        this.appStepper.next();
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
+  payWithJustPay(content: any) {
+    this.basket$.subscribe(basket => {
+      if (!basket?.id) {
+        this.toastr.error('Basket not found');
+        return;
+      }
+
+      // Call your own backend API to create the reference number
+      this.http.get(`https://localhost:5001/api/Payments/billing/${basket.id}`)
+        .subscribe(
+          (response: any) => {
+            console.log('Reference Number Created:', response);
+
+            // Show the modal and start the timer
+            const modalRef = this.modalService.open(content, { centered: true });
+
+            this.timeLeft = 120;
+            this.interval = setInterval(() => {
+              if (this.timeLeft > 0) {
+                this.timeLeft--;
+              } else {
+                clearInterval(this.interval);
+                modalRef.close();
+                this.toastr.warning('Payment time expired');
+              }
+            }, 1000);
+
+            modalRef.result.then(
+              () => clearInterval(this.interval),
+              () => clearInterval(this.interval)
+            );
+          },
+          error => {
+            console.error('Backend Error:', error);
+            this.toastr.error('Failed to connect to backend');
+          }
+        );
+    });
+  }
 }
--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -6,15 +6,24 @@ import { SharedModule } from '../shared/shared.module';
 import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
 import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
 import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
-import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [CheckoutComponent, CheckoutAddressComponent, CheckoutDeliveryComponent, CheckoutReviewComponent, CheckoutSuccessComponent, CheckoutPaymentComponent],
+  declarations: [
+    CheckoutComponent,
+    CheckoutAddressComponent,
+    CheckoutDeliveryComponent,
+    CheckoutReviewComponent,
+    CheckoutPaymentComponent,
+    CheckoutSuccessComponent
+  ],
   imports: [
     CommonModule,
     CheckoutRoutingModule,
-    SharedModule
+    SharedModule,
+    NgbModalModule
   ]
 })
 export class CheckoutModule { }
